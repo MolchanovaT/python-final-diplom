301f4c6f13fb7dbbc5cd0e0148997eeab7cf2a1f
Tatiana Molchanova <tatuka.08@mail.ru>
2024-11-14 13:53:00 -0300

    error in settings.py - redi instead of redis here CELERY_RESULT_BACKEND = 'redis://redis:6379/0'

--- reference/netology_pd_diplom/netology_pd_diplom/settings.py
+++ reference/netology_pd_diplom/netology_pd_diplom/settings.py
@@ -158,5 +158,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_BACKEND = 'redis://redi:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 CELERY_TIMEZONE = 'UTC'
